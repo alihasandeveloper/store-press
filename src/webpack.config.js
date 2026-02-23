3f1b10ed58ac8d09efd171b9d05a56b0cc65288d
Ali Hasan <alihasandeveloper@gmail.com>
2026-02-23 19:47:38 +0600

    react app init

---
+++ src/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development";
+
+  return {
+    entry: "./src/main.jsx",
+
+    output: {
+      path: path.resolve(__dirname, "../admin/build"),
+      filename: "index.js",
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name][ext]",
+          },
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "index.css",
+      }),
+    ],
+
+    devtool: isDev ? "source-map" : false,
+
+    externals: {
+      react: "React",
+      "react-dom": "ReactDOM",
+    },
+  };
+};
